75f29f338ae5439ba63879ee31de9d569dabeb33
yu <kd1338110@st.kobedenshi.ac.jp>
2024-05-14 13:20:57 +0900

    フォーム

--- public/index.php
+++ public/index.php
@@ -1,2 +1,15 @@
-<?php
-phpinfo();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<a href="input.html">text形式の送信練習</a>
+<a href="radio.html">ラジオボタンの送信練習</a>
+<a href="checkbox.html">チェックボックスの送信練習</a>
+<a href="hidden.html">隠し要素フィールド</a>
+
+</body>
+</html>
